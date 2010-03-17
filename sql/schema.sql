113d85fc7e6de0aa12f25c4b98057716d418631f
hkerem <kerem@medratech.com>
2010-03-17 16:45:52 +0200

    Updated SQL schema.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.3.1
+-- http://www.phpmyadmin.net
+--
+-- Anamakine: localhost
+-- Üretim Zamanı: 31 Mart 2010 saat 00:38:42
+-- Sunucu sürümü: 5.1.33
+-- PHP Sürümü: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Veritabanı: `findik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `acl_filter_param`
+--
+
 CREATE TABLE IF NOT EXISTS `acl_filter_param` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `rule_id` int(11) NOT NULL,
@@ -7,12 +34,34 @@ CREATE TABLE IF NOT EXISTS `acl_filter_param` (
   KEY `rule_id` (`rule_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Tablo döküm verisi `acl_filter_param`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `acl_ip_cross`
+--
+
 CREATE TABLE IF NOT EXISTS `acl_ip_cross` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `rule_id` int(10) unsigned NOT NULL,
   `acl_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Tablo döküm verisi `acl_ip_cross`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `acl_rule`
+--
 
 CREATE TABLE IF NOT EXISTS `acl_rule` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -23,12 +72,34 @@ CREATE TABLE IF NOT EXISTS `acl_rule` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Tablo döküm verisi `acl_rule`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `acl_time_cross`
+--
+
 CREATE TABLE IF NOT EXISTS `acl_time_cross` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `rule_id` int(10) unsigned NOT NULL,
   `acl_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Tablo döküm verisi `acl_time_cross`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `blacklist_category`
+--
 
 CREATE TABLE IF NOT EXISTS `blacklist_category` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -37,39 +108,126 @@ CREATE TABLE IF NOT EXISTS `blacklist_category` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Tablo döküm verisi `blacklist_category`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `btk_blacklist`
+--
+
+CREATE TABLE IF NOT EXISTS `btk_blacklist` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `domain` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `domain` (`domain`) USING HASH
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Tablo döküm verisi `btk_blacklist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `btk_db_update`
+--
+
 CREATE TABLE IF NOT EXISTS `btk_db_update` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `update_id` varchar(128) NOT NULL,
-  `update_date` datetime NOT NULL,
+  `update_date` bigint(20) NOT NULL,
   `new_record_count` int(10) unsigned NOT NULL,
   `deleted_record_count` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Tablo döküm verisi `btk_db_update`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `btk_service_status`
+--
 
 CREATE TABLE IF NOT EXISTS `btk_service_status` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `start_date` bigint NOT NULL,
-  `end_date` bigint NOT NULL,
+  `start_date` bigint(20) NOT NULL,
+  `end_date` bigint(20) NOT NULL,
   `status` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
+--
+-- Tablo döküm verisi `btk_service_status`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `btk_timestamp_history`
+--
+
 CREATE TABLE IF NOT EXISTS `btk_timestamp_history` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `start_date` bigint NOT NULL,
-  `end_date` bigint NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
   `log_size` int(10) unsigned NOT NULL,
   `status` smallint(6) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
+--
+-- Tablo döküm verisi `btk_timestamp_history`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `btk_user_info`
+--
+
 CREATE TABLE IF NOT EXISTS `btk_user_info` (
-  `username` varchar(128) NOT NULL,
-  `password` varchar(128) NOT NULL
+  `ts_username` varchar(128) NOT NULL,
+  `ts_password` varchar(128) NOT NULL,
+  `reg_username` varchar(128) NOT NULL,
+  `reg_password` varchar(128) NOT NULL,
+  `update_url` varchar(128) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `btk_user_info` (`username`, `password`) VALUES
-('admin', 'findik');
+--
+-- Tablo döküm verisi `btk_user_info`
+--
+
+INSERT INTO `btk_user_info` (`ts_username`, `ts_password`, `reg_username`, `reg_password`, `update_url`) VALUES
+('tsuser', 'tspass', 'admin', 'findik', 'https://btkupdate.medratech.com/findik-btk/btk-updater.php');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `btk_warning_page`
+--
+
+CREATE TABLE IF NOT EXISTS `btk_warning_page` (
+  `reply_html` text NOT NULL,
+  `time` bigint(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Tablo döküm verisi `btk_warning_page`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `category`
+--
 
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -78,17 +236,20 @@ CREATE TABLE IF NOT EXISTS `category` (
   UNIQUE KEY `catname` (`name`) USING HASH
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-DROP TRIGGER IF EXISTS `categoryinsref`;
+--
+-- Tetiklemeler `category`
+--
+DROP TRIGGER IF EXISTS `findik`.`categoryinsref`;
 DELIMITER //
-CREATE TRIGGER `categoryinsref` AFTER INSERT ON `category`
+CREATE TRIGGER `findik`.`categoryinsref` AFTER INSERT ON `findik`.`category`
  FOR EACH ROW BEGIN
     INSERT INTO category_count VALUES (NEW.id,NEW.name,'0','0','0');
   END
 //
 DELIMITER ;
-DROP TRIGGER IF EXISTS `categorydelref`;
+DROP TRIGGER IF EXISTS `findik`.`categorydelref`;
 DELIMITER //
-CREATE TRIGGER `categorydelref` AFTER DELETE ON `category`
+CREATE TRIGGER `findik`.`categorydelref` AFTER DELETE ON `findik`.`category`
  FOR EACH ROW BEGIN
     DELETE FROM category_count WHERE name = OLD.name;
     DELETE FROM domain WHERE catid = OLD.id;   
@@ -97,6 +258,17 @@ CREATE TRIGGER `categorydelref` AFTER DELETE ON `category`
   END
 //
 DELIMITER ;
+
+--
+-- Tablo döküm verisi `category`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `category_count`
+--
 
 CREATE TABLE IF NOT EXISTS `category_count` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -107,6 +279,17 @@ CREATE TABLE IF NOT EXISTS `category_count` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Tablo döküm verisi `category_count`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `content`
+--
+
 CREATE TABLE IF NOT EXISTS `content` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
@@ -114,22 +297,36 @@ CREATE TABLE IF NOT EXISTS `content` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
-DROP TRIGGER IF EXISTS `contentinsref`;
+--
+-- Tetiklemeler `content`
+--
+DROP TRIGGER IF EXISTS `findik`.`contentinsref`;
 DELIMITER //
-CREATE TRIGGER `contentinsref` AFTER INSERT ON `content`
+CREATE TRIGGER `findik`.`contentinsref` AFTER INSERT ON `findik`.`content`
  FOR EACH ROW BEGIN
     UPDATE category_count SET content_count = content_count + 1 WHERE id = NEW.catid;
   END
 //
 DELIMITER ;
-DROP TRIGGER IF EXISTS `contentdelref`;
+DROP TRIGGER IF EXISTS `findik`.`contentdelref`;
 DELIMITER //
-CREATE TRIGGER `contentdelref` AFTER DELETE ON `content`
+CREATE TRIGGER `findik`.`contentdelref` AFTER DELETE ON `findik`.`content`
  FOR EACH ROW BEGIN
     UPDATE category_count SET content_count = content_count - 1 WHERE id = OLD.catid;
   END
 //
 DELIMITER ;
+
+--
+-- Tablo döküm verisi `content`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `domain`
+--
 
 CREATE TABLE IF NOT EXISTS `domain` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -139,22 +336,36 @@ CREATE TABLE IF NOT EXISTS `domain` (
   UNIQUE KEY `domain` (`domain`) USING HASH
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-DROP TRIGGER IF EXISTS `domaininsref`;
+--
+-- Tetiklemeler `domain`
+--
+DROP TRIGGER IF EXISTS `findik`.`domaininsref`;
 DELIMITER //
-CREATE TRIGGER `domaininsref` AFTER INSERT ON `domain`
+CREATE TRIGGER `findik`.`domaininsref` AFTER INSERT ON `findik`.`domain`
  FOR EACH ROW BEGIN
     UPDATE category_count SET domain_count = domain_count + 1 WHERE id = NEW.catid;
   END
 //
 DELIMITER ;
-DROP TRIGGER IF EXISTS `domaindelref`;
+DROP TRIGGER IF EXISTS `findik`.`domaindelref`;
 DELIMITER //
-CREATE TRIGGER `domaindelref` AFTER DELETE ON `domain`
+CREATE TRIGGER `findik`.`domaindelref` AFTER DELETE ON `findik`.`domain`
  FOR EACH ROW BEGIN
     UPDATE category_count SET domain_count = domain_count - 1 WHERE id = OLD.catid;
   END
 //
 DELIMITER ;
+
+--
+-- Tablo döküm verisi `domain`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `ip_table`
+--
 
 CREATE TABLE IF NOT EXISTS `ip_table` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -165,12 +376,34 @@ CREATE TABLE IF NOT EXISTS `ip_table` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
+--
+-- Tablo döküm verisi `ip_table`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `mime_type`
+--
+
 CREATE TABLE IF NOT EXISTS `mime_type` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `file_ext` varchar(16) NOT NULL,
   `mime_type` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=644 ;
+
+--
+-- Tablo döküm verisi `mime_type`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `mime_type_cross`
+--
 
 CREATE TABLE IF NOT EXISTS `mime_type_cross` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -179,11 +412,33 @@ CREATE TABLE IF NOT EXISTS `mime_type_cross` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=69 ;
 
+--
+-- Tablo döküm verisi `mime_type_cross`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `mime_type_group`
+--
+
 CREATE TABLE IF NOT EXISTS `mime_type_group` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Tablo döküm verisi `mime_type_group`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `time_table`
+--
 
 CREATE TABLE IF NOT EXISTS `time_table` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -194,6 +449,17 @@ CREATE TABLE IF NOT EXISTS `time_table` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
+--
+-- Tablo döküm verisi `time_table`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `url`
+--
+
 CREATE TABLE IF NOT EXISTS `url` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `url` varchar(512) NOT NULL,
@@ -202,22 +468,36 @@ CREATE TABLE IF NOT EXISTS `url` (
   UNIQUE KEY `url` (`url`) USING HASH
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=661056 ;
 
-DROP TRIGGER IF EXISTS `urlinsref`;
+--
+-- Tetiklemeler `url`
+--
+DROP TRIGGER IF EXISTS `findik`.`urlinsref`;
 DELIMITER //
-CREATE TRIGGER `urlinsref` AFTER INSERT ON `url`
+CREATE TRIGGER `findik`.`urlinsref` AFTER INSERT ON `findik`.`url`
  FOR EACH ROW BEGIN
     UPDATE category_count SET url_count = url_count + 1 WHERE id = NEW.catid;
   END
 //
 DELIMITER ;
-DROP TRIGGER IF EXISTS `urldelref`;
+DROP TRIGGER IF EXISTS `findik`.`urldelref`;
 DELIMITER //
-CREATE TRIGGER `urldelref` AFTER DELETE ON `url`
+CREATE TRIGGER `findik`.`urldelref` AFTER DELETE ON `findik`.`url`
  FOR EACH ROW BEGIN
     UPDATE category_count SET url_count = url_count - 1 WHERE id = OLD.catid;
   END
 //
 DELIMITER ;
+
+--
+-- Tablo döküm verisi `url`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo yapısı: `users`
+--
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -226,5 +506,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
-INSERT INTO `users` (`username`, `password`) VALUES
-('admin', '4ff591af51b48e9ca0a7c7dba39cbbd823e39b63');
+--
+-- Tablo döküm verisi `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(4, 'admin', '4ff591af51b48e9ca0a7c7dba39cbbd823e39b63');
